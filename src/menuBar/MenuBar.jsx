f2bcdf34881fe9e1b9230abe05f2e1d2a0b633ef
Dk314053 <dk314053@gmail.com>
2023-05-05 23:29:00 +0530

    day3_menubar_changes

--- src/menuBar/MenuBar.jsx
+++ src/menuBar/MenuBar.jsx
@@ -1,27 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./MenuBar.module.css";
 import File from "../component/MenuFileButton";
 import MessageIcon from "@mui/icons-material/Message";
+import { Tooltip } from '@mui/material'
 import VideoChatIcon from "@mui/icons-material/VideoChat";
 import LockOpenIcon from "@mui/icons-material/LockOpen";
 import Avatar from "@mui/material/Avatar";
 import Edit from "../component/MenuEditButton";
 import ViewButton from "../component/MenuViewButton";
-import { AiOutlineStar } from "react-icons/ai";
-import { MdDriveFileMoveOutline } from "react-icons/md";
-
+import StarIcon from '@mui/icons-material/Star';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+import DriveFileMoveOutlinedIcon from '@mui/icons-material/DriveFileMoveOutlined';
+import CloudDoneOutlinedIcon from '@mui/icons-material/CloudDoneOutlined';
 
 function MenuBar() {
+  const [starr, setStarr] = useState(false)
   const nav = [
-    { id: 3, name: "Insert" },
-    { id: 4, name: "Format" },
-    { id: 5, name: "Tools" },
-    { id: 6, name: "Extensions" },
-    { id: 7, name: "Help" },
+    { id: 1, name: "Insert" },
+    { id: 2, name: "Format" },
+    { id: 3, name: "Tools" },
+    { id: 4, name: "Extensions" },
+    { id: 5, name: "Help" },
   ];
   return (
     <div>
-      <div className={styles.maincontainer}>
+ 
         <img
           className={styles.docimg}
           src="https://cdn-icons-png.flaticon.com/512/5968/5968517.png"
@@ -30,9 +34,19 @@ function MenuBar() {
         <p contentEditable className={styles.mainhead}>
           Untitled document
         </p>
-        <div className={styles.mainimg}>
-          <AiOutlineStar className={styles.star} />
-          <MdDriveFileMoveOutline className={styles.drive} />
+        <div className={styles.lefticon}>
+        <Tooltip title="Star">
+        <p onClick={() => setStarr(!starr)}>
+         {starr ? <StarIcon sx={{ fontSize: "1.5rem", color: '#4285F4' }} /> : <StarOutlineIcon sx={{ fontSize: "1.5rem" }} />}
+         </p>
+        </Tooltip>
+        <Tooltip title="Move">
+        <p><DriveFileMoveOutlinedIcon sx={{ fontSize: "1.5rem" }} /></p>
+          </Tooltip>
+          <Tooltip title="See document status">
+                            <p><CloudDoneOutlinedIcon sx={{ fontSize: "1.5rem" }} /></p>
+                        </Tooltip>
+                        </div>
           <div className={styles.righticon}>
             <MessageIcon className={styles.message} />
             <VideoChatIcon className={styles.video} />
@@ -46,8 +60,8 @@ function MenuBar() {
               alt="Remy Sharp"
               src="https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg"
             />
-          </div>
-        </div>
+         
+  
       </div>
       <ul className={styles.list}>
         <li className={styles.file}>
